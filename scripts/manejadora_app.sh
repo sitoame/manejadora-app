60a8e7621a92bf42f9efa570c6d21c380eecc216
sitoame <amed.murillo@dynateklatam.com>
2026-06-11 10:36:26 -0500

    Logs funcionales en servicio

--- scripts/manejadora_app.sh
+++ scripts/manejadora_app.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /home/dynatek/manejadora_app/my_venv/bin/activate
-exec python3.10 -W 'ignore:semaphore_tracker:UserWarning' -u /home/dynatek/manejadora_app/main.py >> /home/dynatek/manejadora_app/logs/manejadora_app.log 2>&1
+exec python3.12 -W 'ignore:semaphore_tracker:UserWarning' -u /home/dynatek/manejadora_app/main.py >> /home/dynatek/manejadora_app/logs/manejadora_app.log 2>&1
